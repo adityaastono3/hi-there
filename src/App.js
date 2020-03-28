2cce03c97b58d2a1ebea64f67747f19fb4a5461e
Aditya Astono <aditya.astono@debijenkorf.nl>
2020-03-28 23:04:51 +0100

    Animate header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="heading-container animated fadeInDown slow delay-2s">
+          <p className="header-playfair">
+            Hi there,
+          </p>
+          <p className="header-playfair">
+            Yennhi.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

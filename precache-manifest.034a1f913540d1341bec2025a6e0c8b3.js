a16114bf268a07f5c48694b4a40b683a9b2c4784
Aditya Astono <aditya.astono@debijenkorf.nl>
2020-03-29 20:18:15 +0200

    Updates

---
+++ precache-manifest.034a1f913540d1341bec2025a6e0c8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f64690dbf0e242c670fdbda3d8745d",
+    "url": "/hi-there/index.html"
+  },
+  {
+    "revision": "545121a78328a533d790",
+    "url": "/hi-there/static/css/main.3df2528a.chunk.css"
+  },
+  {
+    "revision": "02633295ddb601ed6ca2",
+    "url": "/hi-there/static/js/2.482df7a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hi-there/static/js/2.482df7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545121a78328a533d790",
+    "url": "/hi-there/static/js/main.b2fde260.chunk.js"
+  },
+  {
+    "revision": "12acabfc7c1755e749e7",
+    "url": "/hi-there/static/js/runtime-main.0c84e5f1.js"
+  }
+]);
